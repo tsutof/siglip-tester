807d06324520c119da0314253559a2601702a693
tsutof <47056802+tsutof@users.noreply.github.com>
2024-04-21 17:01:18 +0900

    Updated

--- siglip_collator/__init__.py
+++ siglip_collator/__init__.py
@@ -1,2 +1,1 @@
-from siglip_collator.app import *
 from siglip_collator.model import *
--- siglip_collator/app.py
+++ siglip_collator/app.py
@@ -28,7 +28,7 @@ import requests
 import io
 import pandas as pd
 import random
-import siglip_collator.model as model
+from siglip_collator import *
 
 
 SRC_SEL_NET = "Network :globe_with_meridians:"
@@ -42,7 +42,7 @@ SESS_KEY_LAST_SOURCE = "last_source"
 
 @st.cache_resource
 def get_model():
-    model = model.SiglipModel()
+    model = SiglipModel()
     return model
 
 
@@ -133,7 +133,7 @@ if sel != st.session_state[SESS_KEY_LAST_SOURCE]:
 
 if sel == SRC_SEL_NET:
     # Download an image from internet
-    url = st.text_input('The URL Link')
+    url = st.text_input('Image URL Link')
     if url:
         image = io.BytesIO(requests.get(url).content)
         if image:
